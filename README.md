3c62acb64f532b286c18415d62a325ab64084d33
Michal Pokorný <michal.pokorny0@gmail.com>
2018-06-25 11:46:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pentagon.Extensions.AspNetCore
+
+This repository provides extensions ASP.NET Core library.
+
+## Packages
+
+Package name|Stable                                      
+---|---
+`Pentagon.Extensions.AspNetCore`| [![NuGet][nuget-badge]][nuget]      
+
+
+[nuget]: https://www.nuget.org/packages/Pentagon.Extensions.AspNetCore/
+[nuget-badge]: https://img.shields.io/nuget/v/Pentagon.Extensions.AspNetCore.svg?style=flat-square&label=nuget
---
+++ src/Pentagon.Extensions.AspNetCore/StartupMigration.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="StartupMigration.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Extensions.AspNetCore
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class StartupMigration
+    {
+        const string DefaultSeedName = "seed";
+        const string DefaultMigrationName = "migration";
+
+        bool _isSeedRequested,
+             _isMigrationRequested;
+
+        public string SeedName { get; set; } = DefaultSeedName;
+        public string MigrationName { get; set; } = DefaultMigrationName;
+
+        public bool IsRequested(string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return false;
+            if (args.Contains(MigrationName))
+                _isMigrationRequested = true;
+            if (args.Contains(SeedName))
+                _isSeedRequested = true;
+            return _isMigrationRequested || _isSeedRequested;
+        }
+
+        public async Task RunAsync(IMigrationApp app)
+        {
+            app.OnStartup(null);
+            if (_isMigrationRequested)
+                await app.RunMigrationsAsync();
+            if (_isSeedRequested)
+                await app.EnsureSeedAsync();
+        }
+    }
+}
